1be0afbd398fa4542c270bfea276ada0e9e590f4
Ghazal <kalhorghazal1378@gmail.com>
2023-03-09 20:41:25 +0000

    Update ergm.R

--- ERGM Analysis/ergm.R
+++ ERGM Analysis/ergm.R
@@ -15,6 +15,6 @@ ergmNetwork <- intergraph::asNetwork(graph)
 ergmNetwork
 
 fit <- ergm(ergmNetwork ~ edges + nodematch("Field.of.Interest") + nodematch('Country') + 
-	nodematch('Institute ID') + nodematch('Gender') + nodecov('Citation.Count') + nodecov('h.index'))
+	nodematch('Institute.ID') + nodematch('Gender') + nodecov('Citation.Count') + nodecov('h.index'))
 
 summary(fit)
